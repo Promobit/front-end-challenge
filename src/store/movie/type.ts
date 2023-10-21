c8749ecf174a75c3e72f477e140385881b6b7157
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-21 17:11:23 -0300

    feat: cria type(redux) para recomendação

--- src/store/movie/type.ts
+++ src/store/movie/type.ts
@@ -2,3 +2,4 @@ export const SET_LIST = "SET_LIST";
 export const SET_MOVIE = "SET_MOVIE";
 export const SET_CASTS = "SET_CASTS";
 export const SET_TRAILER = "SET_TRAILER";
+export const SET_RECOMMEDATIONS = "SET_RECOMMEDATIONS";

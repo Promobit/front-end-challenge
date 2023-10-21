7709a0af86b97292920983f6404c1833e6c5875b
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-21 15:12:09 -0300

    feat: adiciona novo endpoint e nova função para buscar creditos do filme

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -7,3 +7,6 @@ export const getPopularMovies = (
 
 export const getMovieById = (movie_id: string) =>
   axiosInstance.get(`movie/${movie_id}`);
+
+export const getCreditsMovie = (movie_id: string) =>
+  axiosInstance.get(`movie/${movie_id}/credits`);

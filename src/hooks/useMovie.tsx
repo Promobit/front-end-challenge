091f04eda0442a52a88f6557c41bf40a9d99df64
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-21 15:12:21 -0300

    feat: adiciona novo endpoint e nova função para buscar creditos do filme

--- src/hooks/useMovie.tsx
+++ src/hooks/useMovie.tsx
@@ -1,11 +1,15 @@
 import { toast } from "react-toastify";
-import { getMovieById, getPopularMovies } from "../api/movie";
+import { getCreditsMovie, getMovieById, getPopularMovies } from "../api/movie";
 import { useDispatch, useSelector } from "react-redux";
 import { setList, setMovie } from "../store/movie/action";
 import { useState } from "react";
 import { RootState } from "../store/store";
 import { Movie } from "../helpers/interfaces/movie";
-import { formatDate } from "../helpers/functions/utils";
+import {
+  convertMinutesToHours,
+  formatDate,
+  formatDateForRelease,
+} from "../helpers/functions/utils";
 
 export const useMovie = () => {
   const list = useSelector((state: RootState) => state.movie.list);
@@ -50,7 +54,31 @@ export const useMovie = () => {
     try {
       if (movie_id) {
         const movieResponse = await getMovieById(movie_id);
+        const credits = (await getCreditsMovie(movie_id)).data;
+
         movieResponse.data.poster_path = `https://image.tmdb.org/t/p/w500${movieResponse.data.poster_path}`;
+        movieResponse.data.release_date = formatDateForRelease(
+          movieResponse.data.release_date
+        );
+        movieResponse.data.runtime = convertMinutesToHours(
+          movieResponse.data.runtime
+        );
+
+        movieResponse.data.jobs = credits.crew
+          .filter(
+            (member: any) =>
+              member.job === "Comic Book" ||
+              member.job === "Director" ||
+              member.job === "Screenplay" ||
+              member.job === "Writer"
+          )
+          .map((member: any) => ({
+            name: member.name,
+            job: member.job,
+          }))
+          .sort((a: any, b: any) => a.job.localeCompare(b.job));
+
+        console.log(movieResponse.data);
         dispatch(setMovie(movieResponse.data));
       }
     } catch (error) {

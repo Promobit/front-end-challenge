85b3ca04a9e2a8546f091cd1ad67c613667ec074
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-21 12:01:07 -0300

    feat: cria tela de filmes com listagem e filtro

--- src/pages/MovieListPage.tsx
+++ src/pages/MovieListPage.tsx
@@ -1,3 +1,45 @@
+import { useEffect } from "react";
+import { Header, MovieFilter, MovieList } from "../components";
+import { useLocation } from "react-router-dom";
+import { getFromSearchParams } from "../helpers/functions/utils";
+import { useGenres, useMovie } from "../hooks";
+import { Pagination } from "../components/shared/pagination";
+
 export const MovieListPage = () => {
-  return <div>MovieList</div>;
+  const location = useLocation();
+  const {
+    handleFetchGenres,
+    genres,
+    genresSelecteds,
+    handleSetGenresSelected,
+  } = useGenres();
+  const { handleFetchAllMovies, movies, pagination } = useMovie();
+
+  useEffect(() => {
+    const genresFromSearch = getFromSearchParams(location.search, "genres");
+    const pageFromSearch = getFromSearchParams(location.search, "page");
+    handleSetGenresSelected(genresFromSearch);
+    handleFetchAllMovies(genresFromSearch, parseInt(pageFromSearch ?? "1"));
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location]);
+
+  useEffect(() => {
+    handleFetchGenres();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <MovieFilter genres={genres} genresSelecteds={genresSelecteds} />
+      <MovieList movies={movies} />
+      <Pagination
+        currentPage={pagination.currentPage}
+        totalPage={pagination.totalPages}
+      />
+    </>
+  );
 };

44f861e26c35f9a55e0e0591f44d86f95f2e673f
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-21 13:41:09 -0300

    feat: adiciona novo action(redux) e refatora

--- src/store/movie/action.ts
+++ src/store/movie/action.ts
@@ -1,7 +1,12 @@
 import { Movie } from "../../helpers/interfaces/movie";
-import { SET_MOVIE } from "./type";
+import { SET_LIST, SET_MOVIE } from "./type";
 
-export const setMovie = (movie: Movie[]) => ({
+export const setList = (movies: Movie[]) => ({
+  type: SET_LIST,
+  payload: movies,
+});
+
+export const setMovie = (movie: any) => ({
   type: SET_MOVIE,
   payload: movie,
 });

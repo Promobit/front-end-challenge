c08f0ae5ceefff6cec446f6699658f084ef4d384
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-21 13:42:23 -0300

    feat: adiciona contrato para detalhes do filme

--- src/helpers/interfaces/movie.ts
+++ src/helpers/interfaces/movie.ts
@@ -14,3 +14,50 @@ export interface Movie {
   vote_average: number;
   vote_count: number;
 }
+
+export interface MovieDetail {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: {
+    id: number;
+    name: string;
+    poster_path: string;
+  };
+  budget: number;
+  genres: Array<{
+    id: number;
+    name: string;
+  }>;
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: Array<{
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }>;
+  production_countries: Array<{
+    iso_3166_1: string;
+    name: string;
+  }>;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: Array<{
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }>;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}

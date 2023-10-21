e7585d2b36bedb750cb5c22e56347f4d46b56a52
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-21 10:23:08 -0300

    feat: cria contrato para filmes

---
+++ src/helpers/interfaces/movie.ts
@@ -0,0 +1,16 @@
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}

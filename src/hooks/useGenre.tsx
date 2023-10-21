e9d3738a7dddb5d6023b8590bbeebb7de92d1448
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-21 10:15:36 -0300

    feat: cria hook personalizado para gêneros

---
+++ src/hooks/useGenre.tsx
@@ -0,0 +1,35 @@
+import { toast } from "react-toastify";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { getGenres } from "../api/genre";
+import { setGenres } from "../store/genre/actions";
+
+export const useGenres = () => {
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(false);
+
+  const genres = useSelector((state: RootState) => state.genres.genres);
+
+  const handleFetchGenres = async () => {
+    try {
+      setLoading(true);
+      const genresResponse = await getGenres();
+      if (genresResponse.status !== 200) {
+        toast.error("Erro ao buscar gêneros!");
+        return false;
+      }
+      dispatch(setGenres(genresResponse.data.genres));
+      setLoading(false);
+    } catch (error) {
+      toast.error("Erro ao buscar gêneros!");
+    }
+  };
+
+  return {
+    genres,
+    loading,
+    handleFetchGenres,
+  };
+};

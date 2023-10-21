c14ce93700e16910305ae20fbed879502216a3a4
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-21 16:01:21 -0300

    feat: cria action(redux) para elenco

--- src/store/movie/action.ts
+++ src/store/movie/action.ts
@@ -1,12 +1,17 @@
-import { Movie } from "../../helpers/interfaces/movie";
-import { SET_LIST, SET_MOVIE } from "./type";
+import { Cast, Movie, MovieDetail } from "../../helpers/interfaces/movie";
+import { SET_CASTS, SET_LIST, SET_MOVIE } from "./type";
 
 export const setList = (movies: Movie[]) => ({
   type: SET_LIST,
   payload: movies,
 });
 
-export const setMovie = (movie: any) => ({
+export const setMovie = (movie: MovieDetail) => ({
   type: SET_MOVIE,
   payload: movie,
+});
+
+export const setCast = (cast: Cast) => ({
+  type: SET_CASTS,
+  payload: cast,
 });
